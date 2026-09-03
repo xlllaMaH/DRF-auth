a594c53815a0c4cf3b8d7db3656066e531fadc5c
Shoma <g.artem1902@gmail.com>
2026-09-03 08:03:39 +0300

    feat(users): create custom user model + manager

--- users/models.py
+++ users/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None, username=None, **extra_fields):
+        if not email:
+            raise ValueError('Укажите электронную почту!')
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('is_staff выключен')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('is_superuser выключен')
+
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
+
+
+class CustomUserModel(AbstractUser):
+    email = models.EmailField(unique=True, max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    username = models.CharField(unique=True ,max_length=128, blank=True, null=True)
+
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+
+    def __str__(self):
+        return self.email
+    
